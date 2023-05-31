27a7929fa3d7c00760ce8e0b1d2090ac7eb9e0d7
GuilhermeAugui <117306676+GuilhermeAugui@users.noreply.github.com>
2023-05-30 21:21:27 -0300

    duplicidade na atividade 1 corrigida

--- lista4.py
+++ lista4.py
@@ -64,69 +64,6 @@ print('O valor do perimetro do triangulo é',tri.calculaPerimetro())
 print('O valor da area do circulo é',cir.calculaArea())
 print('O valor do perimetro do circulo é', cir.calculaPerimetro())
 import math
-class figuraGeometricas():
-    def __init__(self):
-        self._nome= '?'
-        self._tipo= '?'
-class retangulo(figuraGeometricas):
-    def __init__(self, base, altura):
-        self._base= base
-        self._altura= altura
-    def getAltura(self):
-        return self._altura
-    def getBase(self):
-        return self._base
-    def calculaArea(self):
-        area= self._base * self._altura
-        return area 
-    def calculaPerimetro(self):
-        perimetro= self._base + self._base + self._altura + self._altura
-        return perimetro
-class triangulo(figuraGeometricas):
-    def __init__(self, base, altura, lado):
-        self._base = base
-        self._altura= altura
-        self._lado= lado
-    def getAltura(self):
-        return self._altura
-    def getBase(self):
-        return self._base
-    def getLado(self):
-        return self._lado
-    def calculaArea(self):
-        area= (self._base * self._altura)/2
-        return area 
-    def calculaPerimetro(self):
-        perimetro= self._base +self._lado + self._lado   
-        return perimetro
-class circulo(figuraGeometricas):
-    def __init__(self, raio):
-        self._raio= raio     
-    def getRaio(self):
-        return self._raio
-    def calculaArea(self):
-        area= 3.14 * (self._raio)**2
-        return area
-    
-    def calculaPerimetro(self):
-        perimetro= (self._raio *2 *3.14)         
-        return perimetro   
-baseret= float(input('Informe o valor da base do retangulo: '))
-alturaret= float(input('Informe o valor da Altura do retangulo: '))
-basetri= float(input('Informe o valor da base do triangulo: '))
-alturatri= float(input('Informe o valor da altura do triangulo: '))
-ladotri= float(input('Informe o valor do lado do triangulo:'))
-raio= float(input('Informe o valor do raio do circulo: '))
-fg= figuraGeometricas()
-ret= retangulo(baseret,alturaret)    
-tri= triangulo(basetri,alturatri,ladotri)
-cir= circulo(raio)   
-print('O valor da area do retangulo é',ret.calculaArea())
-print('O valor do perimetr do retangulo é',ret.calculaPerimetro())
-print('O valor da area do triangulo é',tri.calculaArea())
-print('O valor do perimetro do triangulo é',tri.calculaPerimetro())
-print('O valor da area do circulo é',cir.calculaArea())
-print('O valor do perimetro do circulo é', cir.calculaPerimetro())
 # 2
 class Veiculo:
     def __init__(self, marca, modelo, ano):
@@ -185,3 +122,4 @@ ca1= Veiculo(CA1marca,CA1modelo,CA1ano)
 ca1=caminhao(CA1marca,CA1modelo,CA1ano,CA1cargaMaxima)
 ca1.carregar()
 ca1.descaregar()
+# 3
